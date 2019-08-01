c9cf24ed9f3fba0ad538babc883494329b23879a
Ramesh <rameshivasoya@gmail.com>
2019-08-01 19:10:03 +0530

    auth component and service created

--- angularPra/src/app/app.module.ts
+++ angularPra/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    AuthComponent
   ],
   imports: [
     BrowserModule,
